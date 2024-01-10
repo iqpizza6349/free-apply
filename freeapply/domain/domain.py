d71ba24e510c06e03af9fb325c576508bc63ea1a
iqpizza6349 <iqpizza6349@gmail.com>
2024-01-10 22:33:42 +0900

    add comment for domain.py

--- freeapply/domain/domain.py
+++ freeapply/domain/domain.py
@@ -1,7 +1,7 @@
 # domain 과 property 를 바탕으로 직접 엔티티를 만들어야함
 # 예를 들어 Domain(name="Person", properties=[Property(name="name", type=str), Property(name="age", type=int)])
 # 위와 같이 정의 되어 있다면, 이를 하나 하나 분해하여 엔티티로 만들고 저장하도록 해야한다.
-"""
+"""freeapply domain module.
 domain.py
 =========
 A function-based module that aims to dynamically create domains.
@@ -12,10 +12,6 @@ Attributes:
 Classes:
     Property: defines each column that use for domain
     DomainDynamicCreation: create domain by dynamic
-
-Todo:
-    * Write comment for this module file
-    * Write comment for each function
 
 author: iqpizza6349
 """
@@ -41,6 +37,21 @@ class DomainDynamicCreation:
         self.metadata = MetaData()
 
     def create_new_domain(self, domain_name: str, properties: List[Property]):
+        """create a new domain by dynamic
+
+        Args:
+            :param domain_name: new domain name
+            :param properties: list of properties
+
+        Returns:
+            :return: nothing.
+            create dynamic domain and no returns.
+
+        Raises:
+            :raise ValueError: if properties are not valid or empty
+
+        """
+
         if properties is None or properties == []:
             raise ValueError("Property list is empty")
 
