ee15ae903e62d2b8483b4e6026fdf0078c65c88e
iqpizza6349 <iqpizza6349@gmail.com>
2024-01-14 17:35:13 +0900

    Add Test code for relationship dynamic tables

--- tests/dynamic_table_creation.py
+++ tests/dynamic_table_creation.py
@@ -5,6 +5,8 @@ from freeapply.domain.domain import DomainDynamicCreation, Property
 
 import unittest
 
+from freeapply.domain.property import ForeignProperty
+
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
@@ -22,6 +24,28 @@ class MyTestCase(unittest.TestCase):
 
         has_domain = self.dynamic_creation.has_domain(domain_name)
         self.assertTrue(has_domain)
+
+    def test_create_domain_with_relation(self):
+        parent = "artist"
+        parent_name = Property(name="name", _type=String)
+
+        child = "song"
+        child_title = Property(name="title", _type=String)
+        child_artist = Property(_type=Integer, name="artist_id")
+
+        self.dynamic_creation.create_new_domain(parent, [parent_name])
+        self.dynamic_creation.create_new_domain(child, [child_title, child_artist])
+
+        has_parent = self.dynamic_creation.has_domain(parent)
+        has_child = self.dynamic_creation.has_domain(child)
+        self.assertTrue(has_parent)
+        self.assertTrue(has_child)
+
+        foreign = ForeignProperty(column="artist_id", ref_column="id")
+        self.dynamic_creation.create_relationship(parent, child, foreign)
+
+        has_relationship = self.dynamic_creation.has_relation(parent, child)
+        self.assertTrue(has_relationship)
 
 
 if __name__ == '__main__':
